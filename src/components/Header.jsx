7ee2b91e858fffd829923ba9be15947e2d7b446b
sohyeon Park <qkrthgus7026.gmail.com>
2024-06-01 21:13:01 +0900

    searchbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 export default function Header(props) {
   let navigate = useNavigate(); //페이지 이동
+  let [keyword, setKeyword] = useState('')
   return (
     <Navbar bg="light" expand="lg" data-bs-theme="light">
       <Container>
@@ -14,6 +16,8 @@ export default function Header(props) {
             <Nav.Link onClick={() => {navigate("/detail");}}> Detail </Nav.Link>
             <Nav.Link onClick={() => {navigate("/about");}}> About </Nav.Link>
             <Nav.Link onClick={() => {navigate("/cart");}}> Cart </Nav.Link>
+            <input onChange={(e)=>{setKeyword(e.target.value)}}></input>
+            {keyword}
           </Nav>
           <Nav className="ms-auto">
             {props.result.isLoading ? '로딩중' : props.result.data.name}
